95db9138bdb8f13d238bdcc90e896e8226a95b7c
vivet <vivet@vivetonline.com>
2026-07-15 16:02:41 +0200

    Updated

--- Cmd.SignUps/Workers/NewUserSignUpWorker.cs
+++ Cmd.SignUps/Workers/NewUserSignUpWorker.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
-using DynamicExpression.Entities;
 using Microsoft.Extensions.Logging;
 using Nano.App.Console.Workers;
 using Svc.Accounts.Models.Api;
-using Svc.Accounts.Models.Criterias;
 using Svc.Emailing.Models.Api;
 using Svc.Emailing.Models.Data;
 using Svc.Emailing.Models.Data.Enums;
-using User = Svc.Accounts.Models.Data.User;
 
 namespace Cmd.SignUps.Workers;
 
@@ -20,18 +16,8 @@ public class NewUserSignUpWorker(ILogger<NewUserSignUpWorker> logger, AccountsAp
     /// <inheritdoc />
     public override async Task OnStartAsync(CancellationToken cancellationToken = default)
     {
-        var users = await accountsApi.Entity
-            .QueryAsync<User, UserQueryCriteria>(new Query<UserQueryCriteria>
-            {
-                Criteria =
-                {
-                    CreatedAfter = DateTimeOffset.UtcNow.AddHours(-2)    
-                },
-                Paging =
-                {
-                    Count = 25
-                }
-            }, cancellationToken);
+        var users = await accountsApi
+            .GetNewSignUpsAsync(cancellationToken);
 
         foreach (var user in users)
         {
